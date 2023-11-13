3737c1aa6024350c19aaa0bfca45e43cd7ae9b14
Oblivious <thanos.papap42@gmail.com>
2023-11-13 15:11:21 +0200

    Setup jest for typescript.

---
+++ spec/spec_helpers.ts
@@ -0,0 +1,21 @@
+import * as Jest from '@jest/globals';
+
+export const expect = Jest.expect;
+
+export const describe = Jest.describe;
+export const fdescribe = Jest.fdescribe;
+export const xdescribe = Jest.xdescribe;
+
+export const context = Jest.describe;
+export const fcontext = Jest.fdescribe;
+export const xcontext = Jest.xdescribe;
+
+export const it = Jest.it;
+export const fit = Jest.fit;
+export const xit = Jest.xit;
+
+export const beforeAll = Jest.beforeAll;
+export const beforeEach = Jest.beforeEach;
+
+export const afterAll = Jest.afterAll;
+export const afterEach = Jest.afterEach;
