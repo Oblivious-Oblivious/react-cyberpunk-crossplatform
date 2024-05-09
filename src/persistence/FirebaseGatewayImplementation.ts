a44773ed2608d752c40a766b6a07bc30401f8725
atha <thanos.papap42@gmail.com>
2024-05-09 16:50:41 +0300

    .

--- src/persistence/FirebaseGatewayImplementation.ts
+++ src/persistence/FirebaseGatewayImplementation.ts
@@ -1,5 +1,5 @@
-import {PersistenceGateway} from './PersistenceGateway.i';
 import {Work} from '../app/uc1_works_loader/Work';
+import {PersistenceGateway} from './PersistenceGateway.i';
 
 export class FirebaseGatewayImplementation implements PersistenceGateway {
   works: Work[] = [];
